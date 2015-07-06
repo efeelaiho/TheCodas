fc0a670ad108ad07fc9f15302bd0ac47f1616e1a
efeelaiho <efeelaiho15@utexas.edu>
2015-07-06 01:34:06 -0500

    created static folders

--- vmax_src/vmax_django/settings.py
+++ vmax_src/vmax_django/settings.py
@@ -101,3 +101,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static", "vmax_static"),
+    #'/var/www/static/',
+)
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root"),
+
+
+
+
+
