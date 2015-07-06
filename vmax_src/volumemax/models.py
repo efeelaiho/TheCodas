d07bf166cdb1bece97017cafd687a48cde909441
Raymond Ton <rtx129@cs.utexas.edu>
2015-07-06 15:55:46 -0500

    added models.py

--- vmax_src/volumemax/models.py
+++ vmax_src/volumemax/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+	first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Albums(models.Model):
+	artist = models.ForeignKey(Artist)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+class TopSongs(models.Model): 
+	artist = models.ForeignKey(Artist)
+	album = models.ForeignKey(Albums)
+	year = models.DateField()
+
+
