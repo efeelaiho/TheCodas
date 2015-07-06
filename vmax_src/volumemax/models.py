dbaed884ad37bafc0f7795e68398b39dcaf91d39
Raymond Ton <rtx129@cs.utexas.edu>
2015-07-06 16:13:16 -0500

    edited models.py

--- vmax_src/volumemax/models.py
+++ vmax_src/volumemax/models.py
@@ -4,11 +4,14 @@ from django.db import models
 class Artist(models.Model):
 	first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    birthplace = models.CharField(max_length=50)
 
 class Albums(models.Model):
 	artist = models.ForeignKey(Artist)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
+    genre = models.CharField(max_length=50)
 
 class TopSongs(models.Model): 
 	artist = models.ForeignKey(Artist)
