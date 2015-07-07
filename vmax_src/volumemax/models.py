b06d665ebc2c1a1d45eafcac614cf59470e7d0ca
Raymond Ton <rtx129@cs.utexas.edu>
2015-07-07 18:56:28 -0500

    added documentation and removed top songs

--- vmax_src/volumemax/models.py
+++ vmax_src/volumemax/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Artist(models.Model):
+	"""
+	each artist will contain first_name, last_name, date_of_birth, and origin
+	date_of_birth will be represented as a datetime object in YYYY/MM/DD format
+	origin will be represented as city, state format (Austin, TX)
+	"""
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	date_of_birth = models.DateField()
@@ -9,14 +13,12 @@ class Artist(models.Model):
 	
 
 class Albums(models.Model):
+	"""
+	each album will contain artist, name, release_date, and genre
+	release_date will be represented as a datetime object in YYYY/MM/DD format
+	"""
 	artist = models.ForeignKey(Artist)
 	name = models.CharField(max_length=100)
 	release_date = models.DateField()
 	genre = models.CharField(max_length=50)
-
-class TopSongs(models.Model): 
-	artist = models.ForeignKey(Artist)
-	album = models.ForeignKey(Albums)
-	year = models.IntegerField()
-
 
