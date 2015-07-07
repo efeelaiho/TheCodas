00d6bbd8d40ce106eb4e84bcad0c3159c12cfdf1
justwinata <justwinata@gmail.com>
2015-07-07 14:22:18 -0500

    Added album art url scraping

--- vmax_src/spotipytest.py
+++ vmax_src/spotipytest.py
@@ -12,34 +12,61 @@ else:
     name = 'Radiohead'
 '''
 
-tempartists = ['7dGJo4pcD2V6oG8kP0tJRR', '19eLuQmk9aCobbVDHc6eek', '4jXfFzeP66Zy67HM2mvIIF', '0kbYTNQb4Pb1rPbbaF0pT4', '3fMbdgg4jU18AjLCKBhRSm', '5K4W6rqBFWDnAN6FQUkS6x']
+tempartists = ['7dGJo4pcD2V6oG8kP0tJRR', '4jXfFzeP66Zy67HM2mvIIF', '5K4W6rqBFWDnAN6FQUkS6x',  '3fMbdgg4jU18AjLCKBhRSm', '0kbYTNQb4Pb1rPbbaF0pT4', '19eLuQmk9aCobbVDHc6eek']
 artistcache = {}
+
+'''
+
+{	Artist Spotify URI: 
+	{
+		'name': artist name, 
+		'img': artist image url, 
+		'albums': 
+			{
+				album name: album art url,
+				...
+			}, 
+		'bio': Wikipedia summary of artist, 
+		'toptracks': 
+			[
+				top track of artist,
+				2nd top track of artist,
+				3rd top track of artist
+			]
+	}
+}
+'''
+
 '''
 imgresults = spotify.search(q='artist:' + name, type='artist')
 items = imgresults['artists']['items']
 if len(items) > 0:
     artist = items[0]
-    '''
+'''
+
 for temp in tempartists:
 	artist = spotify.artist(temp)
-	print (artist)
 	aid = artist['id']
 	artistcache[aid] = {}
+	artistcache[aid]['name'] = artist['name']
+	artistcache[aid]['albums'] = {}
 
 	#image scrape
 	artistcache[aid]['img'] = artist['images'][0]['url']
 
 
-	#albums scrape
+	#albums scrape {album name: album art url}
 	albumresults = spotify.artist_albums(artist['id'], album_type='album')
 	albums = albumresults['items']
 	while albumresults['next']:
 		albumresults = spotify.next(albumresults)
 		albums.extend(albumresults['items'])
-	albumlist = []
 	for album in albums:
-		albumlist.append(album['name'])
-	artistcache[aid]['albums'] = albumlist
+		#albumlist.append(album['name'])
+		if 0 < len(album['images']): 
+			artistcache[aid]['albums'][album['name']] = album['images'][0]['url']
+		else:
+			artistcache[aid]['albums'][album['name']] = 'N/A'
 
 	#top tracks scrape
 	toptracksresults = spotify.artist_top_tracks(artist['id'])
@@ -51,5 +78,6 @@ for temp in tempartists:
 	artistcache[aid]['bio'] = wikipedia.summary(artist['name'])
 
 	#json dump
-	with open ('artistcache.json', 'w') as fp:
-		json.dump(artistcache, fp)
+print (artistcache);
+with open ('artistcache.json', 'w') as fp:
+	json.dump(artistcache, fp)
