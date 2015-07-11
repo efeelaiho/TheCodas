537254f2e63fdc05d82d969172f2e40f5d6be742
efeelaiho <efeelaiho15@utexas.edu>
2015-07-11 12:55:02 -0500

    organize urls.py file by categories

--- vmax_src/vmax_django/urls.py
+++ vmax_src/vmax_django/urls.py
@@ -19,16 +19,36 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    ################################################################### 
+    #
+    #   MAIN NAVBAR
+    #
+    ################################################################### 
 	url(r'^$', 'volumemax.views.home', name='home'),
 	url(r'^about/', 'volumemax.views.about', name='about'),
 	url(r'^artists/', 'volumemax.views.artists', name='artists'),
+    url(r'^albums/', 'volumemax.views.albums', name='albums'),
+    ################################################################### 
+    #
+    #   ARTISTS
+    #
+    ################################################################### 
     url(r'^eminem/', 'volumemax.views.eminem', name='eminem'),
     url(r'^kanyewest/', 'volumemax.views.kanyewest', name='kanyewest'),
     url(r'^michael/', 'volumemax.views.michael', name='michael'),
+    ################################################################### 
+    #
+    #   ALBUMS
+    #
+    ################################################################### 
     url(r'^encore/', 'volumemax.views.encore', name='encore'),
     url(r'^bad/', 'volumemax.views.bad', name='bad'),
     url(r'^college/', 'volumemax.views.college', name='college'),    
-	url(r'^albums/', 'volumemax.views.albums', name='albums'),
+	###################################################################    
+    #
+    #   ADMIN
+    #
+    ################################################################### 
     url(r'^admin/', include(admin.site.urls)),
 
 
