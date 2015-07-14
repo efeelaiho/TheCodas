399edaa10ddd75922dd96836bd2372e08f544d6a
justwinata <justwinata@gmail.com>
2015-07-13 19:02:12 -0500

    Trying to get MySQL connected...

--- vmax_src/vmax_django/settings.py
+++ vmax_src/vmax_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'volumemax'
+    'volumemax'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'vmax_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'volumemax',
+        'USER': 'admin',
+        'PASSWORD': 'thecodas',
+        'HOST': '0e2dcaf09bdc4695b2fbce660612b515882b1ce5.rackspaceclouddb.com',
+      #  'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
