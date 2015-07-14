a4c0c6249a2b0c6682f5e4275e1d296ea1a68e1f
Raymond Ton <rtx129@cs.utexas.edu>
2015-07-14 13:26:33 -0500

    added null = true and blank = true to all fields

--- vmax_src/volumemax/models.py
+++ vmax_src/volumemax/models.py
@@ -8,15 +8,15 @@ class Artist(models.Model):
 	date_of_birth will be represented as a datetime object in YYYY/MM/DD format
 	origin will be represented as city, state format (Austin, TX)
 	"""
-	full_name = models.CharField(max_length=100)
-	date_of_birth = models.DateField()
-	origin = models.CharField(max_length=50)
-	popularity = models.IntegerField()
-	genre = models.CharField(max_length=50)
-	spotify_artist_uri = models.CharField(max_length=500)
-	biography = models.CharField(max_length=5000)
-	youtube_url_1 =models.CharField(max_length=500)
-	youtube_url_2 =models.CharField(max_length=500)
+	full_name = models.CharField(max_length=100,null=True, blank=True)
+	date_of_birth = models.DateField(null=True, blank=True)
+	origin = models.CharField(max_length=50,null=True, blank=True)
+	popularity = models.IntegerField(null=True, blank=True)
+	genre = models.CharField(max_length=50, null=True, blank=True)
+	spotify_artist_uri = models.CharField(max_length=500, null=True, blank=True)
+	biography = models.CharField(max_length=5000, null=True, blank=True)
+	youtube_url_1 = models.CharField(max_length=500, null=True, blank=True)
+	youtube_url_2 = models.CharField(max_length=500, null=True, blank=True)
 
 	def get_absolute_url(self):
 		url_name = self.full_name.replace(' ', '_')
@@ -31,12 +31,12 @@ class Album(models.Model):
 	each album will contain artist, name, release_date, and genre
 	release_date will be represented as a datetime object in YYYY/MM/DD format
 	"""
-	album_artist = models.ForeignKey(Artist)
-	album_name = models.CharField(max_length=100)
-	release_date = models.DateField()
-	genre = models.CharField(max_length=50)
-	spotify_albums_uri = models.CharField(max_length=500)
-	editors_notes = models.CharField(max_length=5000)
+	album_artist = models.ForeignKey(Artist, null=True, blank=True)
+	album_name = models.CharField(max_length=100, null=True, blank=True)
+	release_date = models.DateField(null=True, blank=True)
+	genre = models.CharField(max_length=50, null=True, blank=True)
+	spotify_albums_uri = models.CharField(max_length=500, null=True, blank=True)
+	editors_notes = models.CharField(max_length=5000, null=True, blank=True)
 	
 	def get_absolute_url(self):
 		return "/albums/%s/" % self.album_name
