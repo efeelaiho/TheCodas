1f03fccaea147daa81f4c8073004303acde1dd46
Raymond Ton <rtx129@cs.utexas.edu>
2015-07-14 14:36:32 -0500

    added more fields and updated tests.py

--- vmax_src/volumemax/models.py
+++ vmax_src/volumemax/models.py
@@ -8,15 +8,16 @@ class Artist(models.Model):
 	date_of_birth will be represented as a datetime object in YYYY/MM/DD format
 	origin will be represented as city, state format (Austin, TX)
 	"""
-	full_name = models.CharField(max_length=100,null=True, blank=True)
+	full_name = models.CharField(max_length=100, null=True, blank=True)
 	date_of_birth = models.DateField(null=True, blank=True)
-	origin = models.CharField(max_length=50,null=True, blank=True)
+	origin = models.CharField(max_length=50, null=True, blank=True)
 	popularity = models.IntegerField(null=True, blank=True)
 	genre = models.CharField(max_length=50, null=True, blank=True)
 	spotify_artist_uri = models.CharField(max_length=500, null=True, blank=True)
 	biography = models.CharField(max_length=5000, null=True, blank=True)
 	youtube_url_1 = models.CharField(max_length=500, null=True, blank=True)
 	youtube_url_2 = models.CharField(max_length=500, null=True, blank=True)
+	recommended_album = models.ForeignKey(Album, null = True, blank = True)
 
 	def get_absolute_url(self):
 		url_name = self.full_name.replace(' ', '_')
