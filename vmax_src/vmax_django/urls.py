2853763e3166cf5492ef5ef979006510f17f555d
Mark Laver <mark.laver@utexas.edu>
2015-07-17 16:10:19 -0500

    Cleaned up files, added female artists

--- vmax_src/vmax_django/urls.py
+++ vmax_src/vmax_django/urls.py
@@ -19,14 +19,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+
     ################################################################### 
     #
     #   MAIN NAVBAR
     #
     ################################################################### 
-	url(r'^$', 'volumemax.views.home', name='home'),
-	url(r'^about/', 'volumemax.views.about', name='about'),
-	url(r'^artists/$', 'volumemax.views.artists', name='artists'),
+    url(r'^$', 'volumemax.views.home', name='home'),
+    url(r'^about/', 'volumemax.views.about', name='about'),
+    url(r'^artists/$', 'volumemax.views.artists', name='artists'),
     url(r'^albums/$', 'volumemax.views.albums', name='albums'),
     url(r'^database/$', 'volumemax.views.database', name='database'),
     
@@ -35,21 +36,22 @@ urlpatterns = [
     #   ARTISTS
     #
     ################################################################### 
-    url(r'^eminem/', 'volumemax.views.eminem', name='eminem'),
     url(r'^artists/(\w+)', 'volumemax.views.artist_view', name='artist_view'),
-    url(r'^kanye_west/', 'volumemax.views.kanyewest', name='kanye_west'),
-    url(r'^michael_jackson/', 'volumemax.views.michael', name='michael_jackson'),
+
+    # url(r'^eminem/', 'volumemax.views.eminem', name='eminem'),
+    # url(r'^kanye_west/', 'volumemax.views.kanyewest', name='kanye_west'),
+    # url(r'^michael_jackson/', 'volumemax.views.michael', name='michael_jackson'),
     ################################################################### 
     #
     #   ALBUMS
     #
     ################################################################### 
-    url(r'^encore/', 'volumemax.views.encore', name='encore'),
-    url(r'^bad/', 'volumemax.views.bad', name='bad'),
-    url(r'^college/', 'volumemax.views.college', name='college'),
+    # url(r'^encore/', 'volumemax.views.encore', name='encore'),
+    # url(r'^bad/', 'volumemax.views.bad', name='bad'),
+    # url(r'^college/', 'volumemax.views.college', name='college'),
     url(r'^albums/(\w+)', 'volumemax.views.album_view', name='album_view'),
 
-	###################################################################    
+    ###################################################################    
     #
     #   ADMIN
     #
